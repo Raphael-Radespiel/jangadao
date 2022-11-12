ef53fd4ebfe058de599763ff2165e6bee3d2dd71
Raphael Radespiel <contatoraphael1701@gmail.com>
2022-11-12 16:29:06 -0300

    Finish hamburger menu

--- ham-men.js
+++ ham-men.js
@@ -1,18 +1,34 @@
 const hamburger = document.querySelector(".hamburger-menu");
 const header = document.querySelector("header");
+const content = document.querySelector("#hamburger-content");
+
 hamburger.dataset.isOpen = "false";
 
 hamburger.addEventListener("click", () => {
   if(hamburger.dataset.isOpen == "false"){
-    hamburger.dataset.isOpen = "true";
-    hamburger.src = "./images/bx-x.svg";
-
-    header.style.backgroundColor = "var(--clr-accent)";
+    openHamburger();
   }
   else if(hamburger.dataset.isOpen == "true"){
-    hamburger.dataset.isOpen = "false";
-    hamburger.src = "./images/bx-menu.svg";
-
-    header.style.backgroundColor = "var(--clr-primary)";
+    closeHamburger();
   }
 });
+
+function openHamburger(){
+  hamburger.dataset.isOpen = "true";
+  hamburger.src = "./images/bx-x.svg";
+
+  header.style.backgroundColor = "var(--clr-accent)";
+
+  content.classList.add("content-opened");
+  content.classList.remove("content-closed");
+}
+
+function closeHamburger(){
+  hamburger.dataset.isOpen = "false";
+  hamburger.src = "./images/bx-menu.svg";
+
+  header.style.backgroundColor = "var(--clr-primary)";
+
+  content.classList.remove("content-opened");
+  content.classList.add("content-closed");
+}
